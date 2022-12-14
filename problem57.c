dba18da7619e365758ce060bc9195e73b4abe968
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-14 17:06:23 +0530

    --

---
+++ problem57.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+    int n;
+    printf("Enter the height \n");
+    scanf("%i", &n);
+
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%i", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
