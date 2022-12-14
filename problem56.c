8c0c04d6fa96155c2c0cb87efd51426ce6a6fb15
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-14 16:40:48 +0530

    --

--- problem56.c
+++ problem56.c
@@ -5,24 +5,5 @@
 #include<stdio.h>
 int main(void)
 {
-    int factor = 0;
-
-    for(int i = 2; i <= 100; i++)
-    {
-        for(int j = 1; j <= i; j++)
-        {
-            if(i % j == 0)
-            {
-                factor++;
-            }
-        }
-
-        if(factor == 2)
-        {
-            printf("%i \n", i);
-        }
-        factor = 0;
-    }
-
-    return 0;
+    
 }
