32acebc58a7cde40b9ec35eb6a9757312a2de643
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-14 16:42:28 +0530

    --

--- problem54.c
+++ problem54.c
@@ -18,7 +18,7 @@ int main(void)
     {
         for(int j = 1; j <= i; j++)
         {
-            printf("*");
+            printf("* ");
         }
         printf("\n");
     }
--- problem56.c
+++
@@ -1,9 +0,0 @@
-/*
-    find all prime number between 0 and 100
-*/
-
-#include<stdio.h>
-int main(void)
-{
-    
-}
