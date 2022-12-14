1663983a379ba19eb1c4925e0a6dd160e70907c3
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-14 16:38:43 +0530

    print prime numbers between 1 and 100

--- problem56.c
+++ problem56.c
@@ -1,5 +1,26 @@
+/*
+    find all prime number between 0 and 100
+*/
+
 #include<stdio.h>
 int main(void)
 {
-    
+    int factor = 0;
+
+    for(int i = 2; i <= 100; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            if(i % j == 0)
+            {
+                factor++;
+            }
+        }
+
+        if(factor == 2)
+        {
+            printf("%i \n", i);
+        }
+        factor = 0;
+    }
 }
