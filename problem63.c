0087e6c21f0836e9f0c4bbf5cb47ba9503bcdc0e
Khush Patibandha <khush.patibandha@gmail.com>
2022-12-20 22:25:07 +0530

    Write a program to find the second largest no from user given nos.

---
+++ problem63.c
@@ -0,0 +1,31 @@
+/*
+    Write a program to find the second largest no from user given nos.
+*/
+
+#include<stdio.h>
+int main(void)
+{
+    int num, n, largest = 0, secLargest = 0;
+    
+    printf("How many numbers you would like to enter? \n");
+    scanf("%i", &n);
+
+    for(int i = n; i > 0; i--)
+    {
+        scanf("%i", &num);
+
+        if(num > largest)
+        {
+            secLargest = largest;
+            largest = num;
+        }
+        else
+        {
+            secLargest = num;
+        }
+    }
+
+    printf("Second largest number is %i \n", secLargest);
+
+    return 0;
+}
