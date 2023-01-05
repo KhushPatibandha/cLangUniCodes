4c8d3c50f7c4dc0e553152cc1608648ebd06d8b5
Khush Patibandha <khush.patibandha@gmail.com>
2023-01-05 14:26:10 +0530

    function prac easy

---
+++ problem80.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void add(int, int);
+
+void add(int x, int y)
+{
+    printf("Addition is %i \n", x+y);
+}
+
+int main(void)
+{
+    int a = 10, b = 10;
+    add(a, b);
+
+    return 0;
+}
